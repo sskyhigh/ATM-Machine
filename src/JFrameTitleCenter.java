7a47473ca718a4e60cd9777ad000b4f6790f4471
sskyhigh <windowshao_123@yahoo.com>
2022-10-03 20:32:54 -0400

    Update 19
    Added some codes, indented and formatted some output.

--- src/JFrameTitleCenter.java
+++ src/JFrameTitleCenter.java
@@ -4,37 +4,30 @@ import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
-
 import javax.swing.JFrame;
-
-
 public class JFrameTitleCenter {
 
     public void createAndShowGUI() {
 
         JFrame frame = new JFrame();
         frame.setPreferredSize(new Dimension(300, 200));
-        frame.setTitle("Hello Center");
+        frame.setTitle("Online Banking System");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         frame.addComponentListener(new ComponentAdapter() {
-            @Override
-            public void componentResized(ComponentEvent e) {
-                titleAlign(frame);
-            }
-
-        });
+                                       @Override
+                                       public void componentResized(ComponentEvent e) {
+                                           titleAlign(frame);
+                                       }
+                                   }
+        );
 
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
-
     }
-
     private void titleAlign(JFrame frame) {
-
         Font font = frame.getFont();
-
         String currentTitle = frame.getTitle().trim();
         FontMetrics fm = frame.getFontMetrics(font);
         int frameWidth = frame.getWidth();
@@ -46,5 +39,4 @@ public class JFrameTitleCenter {
         pad = String.format("%" + (spaceCount - 14) + "s", pad);
         frame.setTitle(pad + currentTitle);
     }
-
 }
