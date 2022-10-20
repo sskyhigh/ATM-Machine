0455f72ff1558202ee314fbd73b47938e1e935fd
sskyhigh <windowshao_123@yahoo.com>
2022-10-20 15:32:36 -0400

    Update Deposit.java

--- src/Deposit.java
+++ src/Deposit.java
@@ -1,3 +1,26 @@
-public class Deposit {
-    
+import java.awt.*;
+import java.awt.event.*;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import javax.swing.*;
+import javax.swing.border.BevelBorder;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingUtilities;
+
+public class Deposit implements ActionListener{
+
+    private final JFrame frame = new JFrame("Deposit");
+    private final JLabel message;
+    private final JButton goBack  = new JButton("Go back");
+    private final JButton Deposit = new JButton("Deposit");
+
+    public Deposit(){
+        
+    }
 }
