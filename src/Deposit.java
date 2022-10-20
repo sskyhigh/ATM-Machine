26a9dbc03effb3d19d106228ef6016efe211c6d5
sskyhigh <windowshao_123@yahoo.com>
2022-10-20 15:42:13 -0400

    update

--- src/Deposit.java
+++ src/Deposit.java
@@ -13,14 +13,39 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.SwingUtilities;
 
-public class Deposit implements ActionListener{
+public class Deposit implements ActionListener {
 
-    private final JFrame frame = new JFrame("Deposit");
-    private final JLabel message;
-    private final JButton goBack  = new JButton("Go back");
-    private final JButton Deposit = new JButton("Deposit");
+    private final JFrame frame = new JFrame("Deposit Page");
+    private final JButton goBack = new JButton("Go back");
+    private final JButton DepositButton = new JButton("Deposit");
 
-    public Deposit(){
+    public Deposit() {
+
+        frame.setSize(420, 420);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setVisible(true);
+        frame.setLayout(null);
         
+        goBack.setFocusable(false);
+        goBack.setBounds(70, 75, 120, 35);
+        goBack.setBackground(Color.decode("#238636"));
+        goBack.setForeground(Color.BLACK);
+        goBack.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 18));
+        goBack.addActionListener(this);
+
+        DepositButton.setFocusable(false);
+        DepositButton.setBounds(275, 75, 120, 35);
+        DepositButton.setBackground(Color.decode("#238636"));
+        DepositButton.setForeground(Color.BLACK);
+        DepositButton.setFont(new Font(Font.SANS_SERIF, Font.BOLD, 13));
+        DepositButton.addActionListener(this);
+
+        //frame.add(goBack);
+        frame.add(DepositButton);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
     }
 }
