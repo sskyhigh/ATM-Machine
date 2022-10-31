fccf8e4fefa9d3c9f0ed8283af3ba4ef5e17c135
sskyhigh <98570636+sskyhigh@users.noreply.github.com>
2022-10-31 16:56:41 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ import java.net.URISyntaxException;
 
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
-        Register loginPanel = new Register();
+        FrontPage loginPanel = new FrontPage();
     }
 }
