10bdf41c90c9260a951537ba2aa526cdd39acdaf
sskyhigh <windowshao_123@yahoo.com>
2022-11-20 17:35:30 -0500

    Update

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,9 @@ import java.net.URISyntaxException;
 
 public class Main {
     public static void main(String[] args) throws IOException, URISyntaxException {
-        FrontPage loginPanel = new FrontPage();
+       // FrontPage loginPanel = new FrontPage();
+        for(var i = 0; i<5; ++i){
+            System.out.println("Hello");
+        }
     }
 }
